c85c8fe9a74b2f063739247f0c34a5ea661ab4bf
Unknown <jamesvinicio@gmail.com>
2018-06-04 19:14:15 -0500

    Cambios tareas

--- Codigo_AulaVirtual/tareaNueva.php
+++ Codigo_AulaVirtual/tareaNueva.php
@@ -3,11 +3,13 @@ include 'dbconnection.php';
 include 'redirect.php';
 
 if($_POST){
-  $curso = $_POST["curso"];
+  $curso = $_POST["select_curso"];
   $tema = $_POST["tema"];
   $descripcion = $_POST["descripcion"];
   $fechainicio=$_POST["fechainicio"];
   $fechafin=$_POST["fechafin"];
+  $nrc=$_POST["nrc"];
+  $curso=$_POST["curso"];
   
   $select_max="SELECT MAX(cod_tarea) AS COD_MAX FROM auv_tarea ;";
   $resmax = $mysqli->query($select_max);
@@ -19,6 +21,7 @@ if($_POST){
 
   if ($mysqli->query($sql) === TRUE) {
       echo "Tarea ingresada correctamente";
+      redirect ('tarea.php?Curso='.$curso.'&Nrc='.$nrc);
   } else {
       echo "Error: " . $sql . "<br>" . $mysqli->error;
   }
